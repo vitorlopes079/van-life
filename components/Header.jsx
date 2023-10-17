09d1fa4ddad3bd2f50e8d4e417451695a1d6641b
vitorlopes079 <vitorlopes079@gmail.com>
2023-10-17 09:53:28 +0700

    add google analytics

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,47 +1,62 @@
-import React from "react"
-import { Link, NavLink } from "react-router-dom"
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+import ReactGA from "react-ga";
 
 export default function Header() {
-    const activeStyles = {
-        fontWeight: "bold",
-        textDecoration: "underline",
-        color: "#161616"
-    }
-    
-    function fakeLogOut() {
-        localStorage.removeItem("loggedin")
-    }
-    
-    return (
-        <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
-            <nav>
-                <NavLink 
-                    to="host"
-                    style={({isActive}) => isActive ? activeStyles : null}
-                >
-                    Host
-                </NavLink>
-                <NavLink 
-                    to="about"
-                    style={({isActive}) => isActive ? activeStyles : null}
-                >
-                    About
-                </NavLink>
-                <NavLink 
-                    to="vans"
-                    style={({isActive}) => isActive ? activeStyles : null}
-                >
-                    Vans
-                </NavLink>
-                <Link to="login" className="login-link">
-                    <img 
-                        src="../assets/images/avatar-icon.png" 
-                        className="login-icon"
-                    />
-                </Link>
-                <button onClick={fakeLogOut}>X</button>
-            </nav>
-        </header>
-    )
+  const useAnalyticsEventTracker = (category = "Default Category") => {
+    const eventTracker = (
+      action = "Default Action",
+      label = "Default Label"
+    ) => {
+      ReactGA.event({ category, action, label });
+    };
+    return eventTracker;
+  };
+
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+
+  const gaEventTracker = useAnalyticsEventTracker("Header Nav Click"); // Set the category for header nav clicks
+
+  return (
+    <header>
+      <Link className="site-logo" to="/">
+        #VanLife test
+      </Link>
+      <nav>
+        <NavLink
+          to="host"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          onClick={() => gaEventTracker("Header Nav Click", "Host Link")}
+        >
+          Host
+        </NavLink>
+        <NavLink
+          to="about"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          onClick={() => gaEventTracker("Header Nav Click", "About Link")}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="vans"
+          style={({ isActive }) => (isActive ? activeStyles : null)}
+          onClick={() => gaEventTracker("Header Nav Click", "Vans Link")}
+        >
+          Vans
+        </NavLink>
+        <Link to="login" className="login-link">
+          <img src="../assets/images/avatar-icon.png" className="login-icon" />
+        </Link>
+        <button onClick={fakeLogOut}>X</button>
+      </nav>
+    </header>
+  );
 }
